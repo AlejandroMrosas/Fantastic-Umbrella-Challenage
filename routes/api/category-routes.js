86fc3d6ecfce3dfee245416aee6af60d74b26c79
Alejandro Machado Rosas <alejandromrosas@Alejandros-MacBook-Pro.local>
2021-11-02 13:35:02 -0700

    update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  app.get('/api/categories')
   // be sure to include its associated Products
 });
 
